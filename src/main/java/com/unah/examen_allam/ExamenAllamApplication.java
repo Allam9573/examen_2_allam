9d6d3d8a8f868f1e42e81a2bd05ebced634c8ea7
Allam9573 <aallamdaniel@gmail.com>
2024-03-14 14:45:08 -0600

    first commit

---
+++ src/main/java/com/unah/examen_allam/ExamenAllamApplication.java
@@ -0,0 +1,13 @@
+package com.unah.examen_allam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAllamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAllamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unah/examen_allam/ExamenAllamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unah.examen_allam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAllamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
